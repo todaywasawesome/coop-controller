b3276d50cc9ff6ac1dee5efcf9ff2e9a250142e7
Taras Glek <taras@purestorage.com>
2016-11-22 19:12:29 -0800

    systemd fixing

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 cp coop.service /etc/systemd/system/coop.service
 chmod 664 /etc/systemd/system/coop.service
 systemctl daemon-reload
-systemctl start coop.service
+systemctl enable coop.service #start on startup
+systemctl start coop.service #start now
